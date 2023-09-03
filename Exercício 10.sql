8ddbb832a2d17917c0c90afc2a0a05287861a77a
Thaixxs <thaissilvaferreira13@gmail.com>
2023-09-03 18:48:42 -0300

    Exercício 10 "corrigido"

--- Exercício 10.sql
+++ Exercício 10.sql
@@ -1,2 +1,1 @@
-select * from livros order by autor_id asc;
-select * from livros where autor_id = 1 or autor_id = 4 or autor_id = 5;
+select autor_id, count(*) as quantidade_livro from livros group by autor_id;
