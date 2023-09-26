e1f77592ad924562b17d1af0680f1a2481634b69
Thaixxs <thaissilvaferreira13@gmail.com>
2023-09-25 23:26:09 -0300

    Exercício 03

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -16,3 +16,13 @@ begin
 end;
 $$
 call sp_LivrosPorCategoria('Romance');
+
+-- Exercício 03
+create procedure sp_ContarLivrosPorCategoria(in nome_categoria varchar(100))
+begin
+	declare id_categoria int;
+    select Categoria_ID into id_categoria from categoria where nome_categoria = nome;
+	select count(Categoria_ID) as QuantidadesLivros from livro where Categoria_ID = id_categoria group by Categoria_ID;
+end;
+$$
+call sp_ContarLivrosPorCategoria('Ficção Científica');
