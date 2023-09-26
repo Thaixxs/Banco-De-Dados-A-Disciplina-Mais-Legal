21d674b293711569a0299ab7ad5ed02a0378ae0e
Thaixxs <thaissilvaferreira13@gmail.com>
2023-09-25 23:25:03 -0300

    Exercício 02

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -6,3 +6,13 @@ begin
 end;
 $$
 call sp_ListarAutores();
+
+-- Exercício 02
+create procedure sp_LivrosPorCategoria(in nome_categoria varchar(100))
+begin
+	declare id_categoria int;
+    select Categoria_ID into id_categoria from categoria where nome_categoria = nome;
+	select * from livro where Categoria_ID = id_categoria;
+end;
+$$
+call sp_LivrosPorCategoria('Romance');
